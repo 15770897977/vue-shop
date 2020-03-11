5347de4d70f3bbf452c862a72ce0a1f0ecfe3723
huangYuQing <3200886971@qq.com>
2020-03-11 16:06:29 +0800

    完成Order页面

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import router from './router'
 import './assets/css/global.css'
 // 注意点 : iconfont的样式要放在element的 前面
 import './assets/iconfont/iconfont.css'
+
+// 富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 引入饿了么ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -14,6 +21,7 @@ import TreeTable from 'vue-table-with-tree-grid'
 // axiso 的设置
 import axios from 'axios'
 Vue.use(ElementUI)
+Vue.use(VueQuillEditor)
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 请求拦截器
 axios.interceptors.request.use(config => {
@@ -27,6 +35,21 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 // vue-table-with-tree-grid的注册
 Vue.component('tree-table', TreeTable)
+
+// 全局时间过滤器
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  // yyyy-mm-dd hh:mm:ss
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import List from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(Router)
 
@@ -25,7 +28,10 @@ const router = new Router({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: List },
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order }
       ]
     }
   ]
